f627a3f3599c18f52ba45b06cb1166410dbf031e
Akmal <akmalmavlonov25@gmail.com>
2026-08-24 12:09:24 +0500

    refactor products pages

--- frontend/src/pages/products/MyProductsPage.vue
+++ frontend/src/pages/products/MyProductsPage.vue
@@ -1,12 +1,54 @@
-<script setup lang="ts">
-import MyProductsView from "@/feature/products/MyProductsView.vue";
-</script>
-
 <template>
   <div class="bg-[#F0F0F0] h-screen">
-    <MyProductsView />
+    <Loading v-if="loading" />
+    <div v-else class="xl:px-6 xl:pt-6 lg:px-6 lg:pt-6 md:px-5 md:pt-5 sm:px-4 sm:pt-4 px-4 pt-4">
+      <MainNavBar />
+      <div v-if="myProducts.length > 0" class="flex mt-10">
+        <MyProductsList />
+      </div>
+      <div v-else class="flex justify-center pt-80">
+        <div class="flex flex-col items-center gap-5">
+          <img :src="icon_products" alt="">
+          <div class="flex flex-col gap-2 items-center">
+            <span class="font-bold">You haven't created any product cards</span>
+            <span class="text-[#A3A3A3]">It’s the perfect time to start building your own business.</span>
+            <span class="text-[#A3A3A3]">To do this, go to your useProfile and tap "CREATE PRODUCT COVER".</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Transition name="notify">
+      <DeleteModal v-if="deleteChoice"/>
+    </Transition>
   </div>
 </template>
+
+<script setup lang="ts">
+const { loading } = useBaseModals();
+const { myProducts } = productStore();
+const { getMyProducts } = productsApi();
+const { deleteChoice } = useProfileModals();
+
+import { onMounted } from "vue";
+import { productsApi } from "@/feature/products/api/products.api.ts";
+import { productStore } from "@/feature/products/model/product.store.ts";
+import { useBaseModals } from "@/shared/lib/base.modals.ts";
+import { useProfileModals } from "@/feature/profile/lib/profile.modals.ts";;
+
+import MainNavBar from "@/feature/navigation/ui/MainNavBar.vue";
+import icon_products from "@/assets/icons/products/icon-products.svg";
+import DeleteModal from "@/shared/ui/DeleteModal.vue";
+import MyProductsList from "@/feature/products/ui/MyProductsList.vue";
+import Loading from "@/shared/ui/Loading.vue";
+
+onMounted(async () => {
+  loading.value = true;
+
+  await getMyProducts();
+
+  loading.value = false;
+})
+</script>
 
 <style scoped>
 
--- frontend/src/pages/products/ProductsPage.vue
+++ frontend/src/pages/products/ProductsPage.vue
@@ -1,12 +1,70 @@
-<script setup lang="ts">
-import ProductsView from "@/feature/products/ProductsView.vue";
-</script>
-
 <template>
   <div class="bg-[#F0F0F0] h-screen">
-    <ProductsView />
+    <Loading v-if="loading" />
+    <div v-else-if="componentError" class="flex flex-col items-center justify-center pt-80 p-6 text-red-700 rounded-xl">
+      <span class="text-lg font-semibold mb-2">
+        Something went wrong 😔
+      </span>
+      <p class="text-sm mb-4">{{ componentError }}</p>
+      <button @click="resetError('FILTER')" class="px-4 py-2 mt-5 bg-red-600 text-white rounded-lg hover:bg-red-700 transition-colors">
+        Try again
+      </button>
+    </div>
+    <div v-else class="xl:px-6 xl:pt-6 lg:px-6 lg:pt-6 md:px-5 md:pt-5 sm:px-4 sm:pt-4 px-4 pt-4">
+      <MainNavBar />
+      <div class="flex justify-center mt-12 xl:justify-between">
+        <FilterProducts />
+        <div class="flex flex-col">
+          <HeaderProducts />
+          <ProductList />
+        </div>
+      </div>
+    </div>
+    <Transition name="sidebar">
+      <AsideFilter v-if="filterAside" />
+    </Transition>
+    <Transition name="notify">
+      <Notification v-if="notify" />
+    </Transition>
   </div>
 </template>
+
+<script setup lang="ts">
+const { getAllProducts } = productsApi();
+const { notify, loading } = useBaseModals();
+const { filterAside } = useProductsModals();
+const { componentError, resetError } = errorHandler();
+
+import { onMounted, onErrorCaptured } from "vue";
+import { productsApi } from "@/feature/products/api/products.api.ts";
+import { errorHandler } from "@/shared/lib/errors/error-handler.ts";
+import { useProductsModals } from "@/feature/products/lib/products.modals.ts";
+import { useBaseModals } from "@/shared/lib/base.modals.ts";
+
+import MainNavBar from "@/feature/navigation/ui/MainNavBar.vue";
+import ProductList from "@/feature/products/ui/ProductList.vue";
+import HeaderProducts from "@/feature/navigation/ui/HeaderProducts.vue";
+import FilterProducts from "@/feature/navigation/ui/FilterProducts.vue";
+import Notification from "@/shared/ui/Notification.vue";
+import AsideFilter from "@/shared/ui/AsideFilter.vue";
+import Loading from "@/shared/ui/Loading.vue";
+
+onErrorCaptured((err, info) => {
+  console.error("Перехвачена ошибка в дочернем компоненте:", err);
+  console.log("Детали ошибки:", info);
+
+  componentError.value = "An error occurred while displaying the product catalog."
+
+  return false
+});
+
+onMounted(async () => {
+  loading.value = true;
+
+  await getAllProducts();
+  loading.value = false;
+})
+</script>
 
 <style scoped>
 
