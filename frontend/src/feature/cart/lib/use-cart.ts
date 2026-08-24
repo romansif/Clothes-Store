9504171d115e249b19f488224100d51624ce2453
Akmal <akmalmavlonov25@gmail.com>
2026-08-24 09:40:14 +0500

    fix display checked cart item

--- frontend/src/feature/cart/lib/use-cart.ts
+++ frontend/src/feature/cart/lib/use-cart.ts
@@ -1,10 +1,12 @@
-import type { User } from "@/feature/profile/model/users.types.ts";
-import type { ColorItem, Sizes } from "@/feature/products/model/product.types.ts";
+import { type User } from "@/feature/profile/model/users.types.ts";
+import { type ColorItem, type Sizes } from "@/feature/products/model/product.types.ts";
 import { addToCartForm } from "@/feature/cart/model/cart.forms.ts";
+import { cartStore } from "@/feature/cart/model/cart.store.ts";
 
+const { cart } = cartStore();
 const { cartForm } = addToCartForm();
 
-export const useCartForm = () => {
+export const useCart = () => {
     const addColor = (color: ColorItem, user: User) => {
         if(user.role !== 'Buyer'){
             return
@@ -17,9 +19,15 @@ export const useCartForm = () => {
             return
         }
         cartForm.value.sizes = size.name
-    }
+    };
+
+    const isChecked = (id: string) => {
+        return cart.value.some(f => f.productId === id)
+    };
+
     return {
         addColor,
         addSize,
+        isChecked
     }
 }
