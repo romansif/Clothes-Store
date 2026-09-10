a7073af8c61721c06a331333afba761be6a25dcb
Akmal <akmalmavlonov25@gmail.com>
2026-09-10 10:16:23 +0500

    refactor: add hover for catalog item

--- frontend/src/entities/collection/ui/SeasonsCatalog.vue
+++ frontend/src/entities/collection/ui/SeasonsCatalog.vue
@@ -1,5 +1,6 @@
 <template>
-  <div v-for="season in seasonsCatalog" :key="season.id" class="font-montserrat text-white w-92 h-100 rounded-2xl"
+  <div v-for="season in seasonsCatalog" :key="season.id" class="font-montserrat text-white w-92 h-100 rounded-2xl
+        transition duration-500 hover:scale-108"
        :style="{ backgroundImage: `url(${season.url})` }">
     <div style="filter: drop-shadow(0 0 8px rgba(255, 255, 255, 1))" class="flex flex-col gap-29 px-8 py-12">
       <div class="flex items-center gap-3">
